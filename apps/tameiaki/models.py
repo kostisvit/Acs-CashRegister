4e8242568d083b157cafbddcc74d4f996c9ae2c6
Konstantinos Vitiniotis <kostasvit@gmail.com>
2023-04-21 15:06:38 +0300

    django add pagination

--- apps/tameiaki/models.py
+++ apps/tameiaki/models.py
@@ -15,22 +15,3 @@
 #         verbose_name = 'Ταμειακές'
 #         verbose_name_plural = 'Ταμειακές'
 #         ordering = ['customer']
-
-
-
-
-
-
-# class Customer(models.Model):
-#     first_name = models.CharField(max_length=100,null=True, blank=True)
-#     last_name = models.CharField(max_length=150,null=True, blank=True)
-#     company_name = models.CharField(max_length=150,null=True, blank=True)
-#     company_type = models.CharField(max_length=150,null=True, blank=True)
-#     company_address = models.CharField(max_length=150,null=False, blank=False)
-#     company_afm = EncryptedCharField(max_length=150,null=False, blank=False)
-#     phone_number = models.CharField(max_length=150,null=False, blank=False)
-
-#     class Meta:
-#         verbose_name = 'Πελάτης'
-#         verbose_name_plural = 'Πελάτης'
-#         ordering = ['last_name']
--- apps/tameiaki/views.py
+++ apps/tameiaki/views.py
@@ -14,19 +14,30 @@
 
 
 from django.shortcuts import render
-from django.http import HttpResponse
 import requests
-from django.utils.safestring import SafeString
 import json
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
 # Create your views here.
 def customers(request):
     #pull data from third party rest api
     #response = requests.get('https://jsonplaceholder.typicode.com/users')
-    response = requests.get('http://127.0.0.1:9999/customers')
+    response = requests.get('http://127.0.0.1:8280/customers')
     #convert reponse data into json
     data = json.loads(response.content)
+    paginator = Paginator(data, 12) # 3 posts in each page
+    data = request.GET.get('page')
+    try:
+        data = paginator.page(data)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        data = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        data = paginator.page(paginator.num_pages)
     context = {
         'data': data,
+        'page': data,
     }
-    print(customers)
+    print(context)
     return render(request, "customer.html", context)
