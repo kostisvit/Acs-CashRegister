3e14a3acafd0806e78f4c5d3be23b862dcecad94
Konstantinos Vitiniotis <kostasvit@gmail.com>
2023-05-08 10:56:49 +0300

    tameieki add status filter

--- apps/tameiaki/filters.py
+++ apps/tameiaki/filters.py
@@ -1,9 +1,14 @@
+from django import forms
 import django_filters
 from .models import Cash
 
 class CashFilter(django_filters.FilterSet):
+    
+    CHOICES = ((True, 'Online'), (False, 'Offline'))
+    
     customer = django_filters.CharFilter(lookup_expr='icontains', label='Πελάτης')
     cash_model = django_filters.CharFilter(lookup_expr='icontains', label='Ταμειακή')
+    status = django_filters.ChoiceFilter(choices=CHOICES, label='Κατάσταση')
     class Meta:
         model = Cash
-        fields = ['customer','cash_model']
+        fields = ['customer','cash_model','status']
