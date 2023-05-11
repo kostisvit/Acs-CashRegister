209810f7ebc00aad997a01293a30c910ea58bf12
kostasvit <kostasvit@gmail.com>
2023-05-11 21:47:55 +0300

    css changes

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CaptchaLoginView
-from .views import SignUpView
+from .views import CaptchaLoginView,SignUpView
+
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', CaptchaLoginView.as_view(), name='login'),
 ]
