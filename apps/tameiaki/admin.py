fb33af178dc34236703e8a6dafe7829533b87339
kostasvit <kostasvit@gmail.com>
2023-10-01 08:40:55 +0300

    admin show created date info

--- apps/tameiaki/admin.py
+++ apps/tameiaki/admin.py
@@ -7,7 +7,7 @@ class CashAdmin(ImportExportModelAdmin):
     list_display = ('customer', 'cash_model', 'cash_number','register_date', 'old_os', 'new_os', 'update_date','status','info','created_at','updated_at')
 
 class FileAdmin(ImportExportModelAdmin):
-    list_display = ('id','customer','file')
+    list_display = ('id','customer','file','created_at')
 
 
 admin.site.register(Cash,CashAdmin)
