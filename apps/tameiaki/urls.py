eb149c359f149d8052b2cf069b9bd0af9274e878
kostasvit <kostasvit@gmail.com>
2023-10-25 21:29:50 +0300

    fixing urls

--- apps/tameiaki/urls.py
+++ apps/tameiaki/urls.py
@@ -8,15 +8,15 @@ from .views import CashExport,Export_data,export_data_as_excel,CustomerFormView,
 
 
 urlpatterns = [
-    path('cash-register/customer-api', views.customers, name='customer'),
-    path('cash-register/customer-new', CustomerFormView.as_view(), name='customer-new'),
+    path('cash-register/api/customers', views.customers, name='customer'),
+    path('cash-register/api/customer/new', CustomerFormView.as_view(), name='customer-new'),
     path('cash-register/cash', views.TameiakiFilterView.as_view(), name='tameiaki'),
-    path('cash-register/new-cash', views.CreatePostView.as_view(), name='new-cash'),
+    path('cash-register/cash/new', views.CreatePostView.as_view(), name='new-cash'),
     path('cash-register/files', views.FileListView.as_view(), name='files'),
     path('cash-register/upload-fille/', FileUploadView.as_view(), name='file_upload'),
     # update
-    path('cash-register/update/<uuid:pk>', views.CashUpdateView.as_view(), name='edit-cash'),
+    path('cash-register/cash/update/<uuid:pk>', views.CashUpdateView.as_view(), name='edit-cash'),
     # export
-    path('cash-register/export/cash', views.Export_data, name='export_data'),
-    path('cash-register/export/customers', views.export_data_as_excel, name='export_data_client'),
+    path('cash-register/cash/export', views.Export_data, name='export_data'),
+    path('cash-register/customers/export', views.export_data_as_excel, name='export_data_client'),
 ]
